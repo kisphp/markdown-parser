ef82a1cd8c03e1e608c9340833697ab2c8262695
Marius Rizac <marius.rizac@spryker.com>
2016-04-10 10:13:32 +0200

    added block skip

---
+++ src/Blocks/BlockSkip.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Kisphp\Blocks;
+
+use Kisphp\AbstractBlock;
+
+class BlockSkip extends AbstractBlock
+{
+    /**
+     * @return null
+     */
+    public function parse()
+    {
+        return null;
+    }
+
+    /**
+     * @return null
+     */
+    public function getStartTag()
+    {
+        return null;
+    }
+
+    /**
+     * @return null
+     */
+    public function getEndTag()
+    {
+        return null;
+    }
+}
