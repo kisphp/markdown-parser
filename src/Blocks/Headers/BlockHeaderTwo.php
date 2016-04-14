622115f5bbc97b47818b7a8d3fbd1fde656f242c
Marius Rizac <marius.rizac@spryker.com>
2016-04-14 23:11:58 +0200

    removed unused code from BlockHeaderTwo

--- src/Blocks/Headers/BlockHeaderTwo.php
+++ src/Blocks/Headers/BlockHeaderTwo.php
@@ -2,10 +2,6 @@
 
 namespace Kisphp\Blocks\Headers;
 
-use Kisphp\Blocks\BlockEmpty;
-use Kisphp\BlockTypes;
-use Kisphp\DataObjectInterface;
-
 class BlockHeaderTwo extends AbstractBlockSpecialHeader
 {
     /**
@@ -30,35 +26,5 @@ class BlockHeaderTwo extends AbstractBlockSpecialHeader
     public function getEndTag()
     {
         return '</h2>';
-    }
-
-    /**
-     * @param DataObjectInterface $dataObject
-     *
-     * @return BlockInterface
-     */
-    public function changeLineType2(DataObjectInterface $dataObject)
-    {
-        $previousLine = $this->lineNumber - 1;
-        if (!$dataObject->hasLine($previousLine)) {
-            return $this;
-        }
-
-        $previousLineObject = $dataObject->getLine($previousLine);
-        if (is_a($previousLineObject, BlockEmpty::class)) {
-            $guess = $this->factory->createRowTypeGuesser();
-            if ($guess->isBlockHorizontalRule($this->content)) {
-                $newObject = $this->factory->create(BlockTypes::BLOCK_HORIZONTAL_RULE)
-                    ->setContent($this->content)
-                    ->setLineNumber($this->lineNumber)
-                ;
-
-                $dataObject->updateLine($this->lineNumber, $newObject);
-
-                return $newObject;
-            }
-        }
-
-        return parent::changeLineType($dataObject);
     }
 }
