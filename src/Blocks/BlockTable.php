913fa65f863b86dc08d8e7fac2433f163640dea0
Kisphp <mariusbogdan83@gmail.com>
2016-04-19 20:16:52 +0000

    render tables

--- src/Blocks/BlockTable.php
+++ src/Blocks/BlockTable.php
@@ -3,21 +3,168 @@
 namespace Kisphp\Blocks;
 
 use Kisphp\AbstractBlock;
+use Kisphp\AbstractBlockNoParse;
+use Kisphp\BlockInterface;
+use Kisphp\BlockTypes;
+use Kisphp\DataObjectInterface;
 
-class BlockTable extends AbstractBlock
+class BlockTable extends AbstractBlockNoParse
 {
-    public function parse()
+    /**
+     * @return string
+     */
+    public function getStartTableTag()
     {
-        return $this->content;
+        return '<table>';
     }
 
-    public function getStartTag()
+    /**
+     * @return string
+     */
+    public function getEndTableTag()
     {
-        // TODO: Implement getStartTag() method.
+        return '</table>';
     }
 
-    public function getEndTag()
+    /**
+     * @param DataObjectInterface $dataObject
+     */
+    public function changeLineType(DataObjectInterface $dataObject)
     {
-        // TODO: Implement getEndTag() method.
+        $max = $dataObject->count();
+        $changeNextLine = true;
+        $firstLineCompiled = false;
+
+        $htmlTable = $this->getStartTableTag();
+
+        for ($i = $this->lineNumber; $i < $max; $i++) {
+            if ($firstLineCompiled === false) {
+                $firstLineCompiled = true;
+                $previousLineIndex = $i - 1;
+                $previousLine = $dataObject->getLine($previousLineIndex);
+                $htmlTable .= $this->createTableRow($previousLine, true);
+                $this->createSkipLine($dataObject, $previousLineIndex);
+                unset($previousLineIndex);
+            }
+
+            $currentLine = $dataObject->getLine($i);
+
+            $htmlTable .= $this->createTableRow($currentLine);
+
+            $this->createSkipLine($dataObject, $i);
+
+            /** @var AbstractBlock $nextLineObject */
+            $nextLineObject = $dataObject->getLine($i + 1);
+            if (!$this->isTableLineType($nextLineObject)) {
+                $changeNextLine = false;
+            }
+
+            if ($changeNextLine === false) {
+                break;
+            }
+        }
+
+        $htmlTable .= $this->getEndTableTag();
+
+        $htmlTable = $this->parseInlineMarkup($htmlTable);
+
+        $listContent = $this->factory->create(BlockTypes::BLOCK_UNCHANGE)
+            ->setContent($htmlTable)
+            ->setLineNumber($this->lineNumber)
+        ;
+
+        $dataObject->updateLine($this->lineNumber, $listContent);
+    }
+
+    /**
+     * @param DataObjectInterface $dataObject
+     * @param int $lineNumber
+     */
+    protected function createSkipLine(DataObjectInterface $dataObject, $lineNumber)
+    {
+        $changedContent = $this->factory->create(BlockTypes::BLOCK_SKIP);
+        $dataObject->updateLine($lineNumber, $changedContent);
+    }
+
+    /**
+     * @param BlockInterface $block
+     *
+     * @return string
+     */
+    protected function createTableRow(BlockInterface $block, $isHeader = false)
+    {
+        $lineContent = trim($block->getContent(), '|');
+
+        if (strpos($lineContent, '---') !== false) {
+            return '';
+        }
+
+        $tableRow = explode('|', $lineContent);
+
+        $rowHtml = $this->getTableRowStartTag();
+
+        foreach ($tableRow as $rowIndex => $row) {
+            $rowHtml .= $this->getTableColumnStartTag($isHeader);
+            $rowHtml .= trim($row);
+            $rowHtml .= $this->getTableColumnEndTag($isHeader);
+        }
+
+        $rowHtml .= $this->getTableRowEndTag();
+
+        return $rowHtml;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getTableRowStartTag()
+    {
+        return '<tr>';
+    }
+
+    /**
+     * @return string
+     */
+    protected function getTableRowEndTag()
+    {
+        return '</tr>';
+    }
+
+    /**
+     * @param bool|false $isTableHeader
+     *
+     * @return string
+     */
+    protected function getTableColumnStartTag($isTableHeader = false)
+    {
+        if ($isTableHeader === true) {
+            return '<th>';
+        }
+
+        return '<td>';
+    }
+
+    /**
+     * @param bool|false $isTableHeader
+     *
+     * @return string
+     */
+    protected function getTableColumnEndTag($isTableHeader = false)
+    {
+        if ($isTableHeader === true) {
+            return '</th>';
+        }
+
+        return '</td>';
+    }
+
+    /**
+     * @param BlockInterface $block
+     *
+     * @return bool
+     */
+    protected function isTableLineType(BlockInterface $block)
+    {
+        return ($this->lineIsObjectOf($block, static::class) || strpos($block->getContent(), '|') !== false);
     }
 }
